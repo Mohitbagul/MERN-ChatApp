b304efe1c0c1a8526ca7f91e766a1294dfaa24ce
Mohit Bagul <126978039+Mohitbagul@users.noreply.github.com>
2025-02-28 22:51:55 +0530

    Update Users.js

--- live-chat-client/src/Components/Users.js
+++ live-chat-client/src/Components/Users.js
@@ -34,7 +34,7 @@ function Users() {
         Authorization: `Bearer ${userData.data.token}`,
       },
     };
-    axios.get("http://localhost:8080/user/fetchUsers", config).then((data) => {
+    axios.get("mern-chat-app-api-nine.vercel.app/user/fetchUsers", config).then((data) => {
       console.log("UData refreshed in Users panel ");
       setUsers(data.data);
       // setRefresh(!refresh);
