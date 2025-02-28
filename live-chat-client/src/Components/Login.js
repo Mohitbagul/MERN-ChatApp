03532b793c78169e32dac8a8b07f8853ed87b9b4
Mohit Bagul <126978039+Mohitbagul@users.noreply.github.com>
2025-02-28 22:49:35 +0530

    Update Login.js

--- live-chat-client/src/Components/Login.js
+++ live-chat-client/src/Components/Login.js
@@ -30,7 +30,7 @@ function Login() {
       };
 
       const response = await axios.post(
-        "http://localhost:8080/user/login/",
+        "mern-chat-app-api-nine.vercel.app/user/login/",
         data,
         config
       );
@@ -58,7 +58,7 @@ function Login() {
       };
   
       const response = await axios.post(
-        "http://localhost:8080/user/register/",
+        "mern-chat-app-api-nine.vercel.app/user/register/",
         data,
         config
       );
